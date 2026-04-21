7002413eb8c0670cca237136bcfbd506f0e157c5
Maida Gómez <maidagompin@gmail.com>
2026-04-21 14:49:24 -0300

    Actualizar la conexión a la base de datos y añadir configuración para Vercel

--- Back-end/config/Basedatos.js
+++ Back-end/config/Basedatos.js
@@ -5,7 +5,7 @@ const url = "mongodb://localhost:27017/personas";
 
 export const conectar = async () => {
     try {
-        await mongoose.connect(url);
+        await mongoose.connect(`mongodb+srv://Vercel-Admin-Proyecto_final_MongoDB:hthktGJ0jJXdCl8O@proyecto-final-mongodb.d79ic8d.mongodb.net/?retryWrites=true&w=majority`);
         console.log("Conectado a la Base de datos");
     } catch (error) {
         console.error("Error al conectar a la Base de datos", error.message);
